e7be820db858e35197092b6f9d45a7b90db2412f
Anusha0317 <anushareddys05170@gmail.com>
2026-03-19 12:07:59 +0000

    Completed add parcel functionality with dynamic list update .own code

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,11 @@
 function addParcel() {
     let name =
     document.getElementById("parcelName").ariaValueMax;
+    let status =
+    document.getElementById("status").ariaValueMax;
+    let li =
+    document.createElement("li");li.textContent = name + " - " +status;
+    
+    document.getElementById("parcelList").appendChild(li);
+    console.log("Parecel added successfully");
 }
